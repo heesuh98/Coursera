b901450c9cf7406214bda91488eb4f56c44b8e17
Heesuh Cho <heesuh98@gmail.com>
2021-01-27 00:18:32 +0900

    QUIZ_2

---
+++ Bioinformatics/Forward-reverse half strand.py
@@ -0,0 +1,22 @@
+pattern = "GAGCCACCGCGATA"
+
+
+def gc_skew(pattern):
+    skew = 0
+
+    print(skew, end=" ")
+
+    for nu in pattern:
+
+        if nu == "C":
+
+            skew -= 1
+
+        elif nu == "G":
+
+            skew += 1
+
+        print(skew, end=" ")
+
+
+gc_skew(pattern)
